4679ceb1411b7b03df86500034fb81540444dcb6
cccaizsp <92281194+cccaizsp@users.noreply.github.com>
2025-05-15 01:22:57 +0800

    test
    padapada发布测试

---
+++ src/settings.43511.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/pvScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"ef8da",main:"5c973"}};
