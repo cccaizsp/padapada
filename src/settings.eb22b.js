66f12d17ed815cdc44e2fc2c271ae3b1e2b4df0f
cccaizsp <92281194+cccaizsp@users.noreply.github.com>
2025-05-18 23:55:16 +0800

    5.18-last ver

---
+++ src/settings.eb22b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/pvScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"c8de2",main:"e6388"}};
